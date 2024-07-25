920914d46e00a4b15f1b50637d909e6cd5665d17
SAMUEL GOMES PEREIRA JUNIOR <152228870+SamuelJunr@users.noreply.github.com>
2024-07-25 12:53:39 -0300

    Update Dio_specialist.sql

--- Dio_specialist.sql
+++ Dio_specialist.sql
@@ -48,3 +48,25 @@ CREATE INDEX idx_employee_FullName ON employee((concat(Fname, " ", Minit, " ", L
 CREATE INDEX idx_employee_Fname ON employee(Fname);
 CREATE INDEX idx_employee_Minit ON employee(Minit);
 CREATE INDEX idx_employee_Lname ON employee(Lname);
+
+
+------------------------------------------------------
+--procedure para um ecommerce
+------------------------------------------------------
+
+USE ecommerce;
+
+CREATE PROCEDURE CRUD(
+	IN opcao int, IN id int
+)
+BEGIN	
+	CASE opcao		
+		WHEN 1 THEN
+			SELECT * FROM orders WHERE idOrder = id ; -- Mostrar order
+        WHEN 2 THEN
+			UPDATE orders SET orderStatus = 'Confirmado' WHERE idOrder = id; -- confirmar order
+		WHEN 3 THEN
+			DELETE FROM productOrder WHERE idPOorder = id; -- excluir produtos da order
+			DELETE FROM orders WHERE idOrder = id; -- excluir order
+	END CASE;
+END 
