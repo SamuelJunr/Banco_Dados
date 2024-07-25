4eb623f9b680161a7c7b2d5afa1b37e98cdfb4c8
SAMUEL GOMES PEREIRA JUNIOR <152228870+SamuelJunr@users.noreply.github.com>
2024-07-25 13:17:52 -0300

    Update transacao.sql

--- tecnica_avancada/transacao.sql
+++ tecnica_avancada/transacao.sql
@@ -1,1 +1,42 @@
+DELIMITER //
 
+CREATE PROCEDURE minha_procedure() 
+  BEGIN 
+-- Declarar variáveis 
+  DECLARE exit_handler BOOLEAN DEFAULT FALSE;
+DECLARE rollback_handler BOOLEAN DEFAULT FALSE;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET exit_handler = TRUE;
+
+-- Desabilitar o autocommit
+SET autocommit = 0;
+
+-- Iniciar a transação
+START TRANSACTION;
+
+-- Definir savepoint para rollback parcial (opcional)
+SAVEPOINT savepoint1;
+
+-- Executar instruções SQL (exemplo)
+INSERT INTO tabela (coluna1, coluna2) VALUES (valor1, valor2);
+UPDATE tabela SET coluna1 = novo_valor WHERE condição;
+DELETE FROM tabela WHERE condição;
+-- E assim por diante...
+
+-- Verificar se ocorreu algum erro durante a execução das instruções
+IF exit_handler THEN
+    -- Desfazer até o savepoint (rollback parcial) ou desfazer a transação inteira (rollback total)
+    IF rollback_handler THEN
+        ROLLBACK TO savepoint1;
+    ELSE
+        ROLLBACK;
+    END IF;
+ELSE
+    -- Confirmar a transação
+    COMMIT;
+END IF;
+
+-- Reabilitar o autocommit
+SET autocommit = 1;
+END //
+
+DELIMITER ;
